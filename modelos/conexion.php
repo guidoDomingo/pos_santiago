3fa75b92307e176e55b9bb52fedae8806f5bf15c
Guido Ruiz <ruizbenitezguido11@gmail.com>
2022-12-28 21:17:46 -0300

    Actualizado

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,7 +5,7 @@
 	 	static public function conectar(){
 
 	 		$link = new PDO("mysql:host=localhost;dbname=unclzepr_posFactura"
-	 						,"unclzepr_guido","$Guido$123");
+	 						,"unclzepr_guido","guidoruiz123");
 
 	 		$link->exec("set names utf8");
 
